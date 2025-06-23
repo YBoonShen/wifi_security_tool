64ddfa5a51ef6152552931d6f55b9cf62cf63b0d
YEAP BOON SHEN <boonshen1159@gmail.com>
2025-06-23 13:57:13 +0800

    "1"

---
+++ 02.py
@@ -0,0 +1,29 @@
+import pywifi  # pyright: ignore[reportMissingImports]
+from pywifi import const  # pyright: ignore[reportMissingImports]
+import time
+#https://github.com/microsoft/pylance-release/blob/main/docs/diagnostics/reportMissingImports.md
+# Use comments like # pyright: ignore[reportMissingImports] to suppress warnings for optional or platform-specific imports.
+
+#get the wifi card
+
+def get_card():
+    wifi = pywifi.PyWiFi()
+    card = wifi.interfaces()[0]  # Get the first wifi card
+    card.disconnect
+    time.sleep(1)
+    status = card.status()
+    if status not in [const.IFACE_DISCONNECTED, const.IFACE_INACTIVE]:
+        print("Disconnecting from current network...")
+        return False
+    return card
+
+# Scan for available networks
+def scan_wifi(card):
+    print("Scanning for networks...")
+    card.scan()
+    time.sleep(15)  # Wait for the scan to complete
+    wift_list = card.scan_results()
+    print(wift_list)
+card = get_card()
+scan_wifi(card)
+# Note: The above code assumes that the pywifi library is installed and available in your Python environment.    
